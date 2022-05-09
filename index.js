1586ec72f9f8212e643cfa4ab5371bf82dc71eef
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-09 08:41:05 +0100

    Add context

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const canvas = document.getElementById('mainCanvas')
 canvas.height = window.innerHeight;
 canvas.width = 200;
+
+//get drawing context
+const context = canvas.getContext('2d');
+//get car
+const car = new Car(100, 100, 30, 50);
+car.draw(context);
