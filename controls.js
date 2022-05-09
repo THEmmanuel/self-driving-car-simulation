d0047dcf9fa94ddd4577551264ff75d163fab91e
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-09 09:02:32 +0100

    set up keypress listener

--- controls.js
+++ controls.js
@@ -4,5 +4,33 @@ class Controls {
 		this.left = false;
 		this.right = false;
 		this.backward = false;
+
+		// # in front because this is a private method. Can't be accesed outside of this class.
+		this.#addKeyboardListeners();
+	}
+
+	#addKeyboardListeners() {
+		document.onkeydown = event => {
+			switch (event.key) {
+				case 'ArrowLeft':
+					this.left = true
+					break;
+
+				case 'ArrowRight':
+					this.right = true
+					break;
+
+				case 'ArrowUp':
+					this.forward = true
+					break;
+
+				case 'ArrowDown':
+					this.backward = true
+					break;
+
+				default:
+					break;
+			}
+		}
 	}
 }
