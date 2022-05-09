4c6607099db649baef15765e9349c7ad59d15fff
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-09 13:43:49 +0100

    render road

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ canvas.width = 200;
 const context = canvas.getContext('2d');
 //get car
 const car = new Car(100, 100, 30, 50);
+//get road
+const road = new Road(canvas.width/2, canvas.width*0.9);
 car.draw(context);
 
 const animate = () => {
 	car.update();
 	canvas.height = window.innerHeight;
+	road.draw(context);
 	car.draw(context)
 	//Calls the animate 
 	requestAnimationFrame(animate)
