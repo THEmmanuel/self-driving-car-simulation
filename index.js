4691c07b1606e1928119ce94568fdee6956dc582
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-09 16:45:46 +0100

    Add camera trail

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 const animate = () => {
 	car.update();
 	canvas.height = window.innerHeight;
+	context.save();
+
+	context.translate(0, -car.y+canvas.height * 0.7);
+
 	road.draw(context);
-	car.draw(context)
+	car.draw(context);
+
+	context.restore();
 	//Calls the animate 
-	requestAnimationFrame(animate)
+	requestAnimationFrame(animate);
 }
 
 animate();
