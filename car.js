e8e52736b390d2ee10a6c80c77f1bb123fc05761
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-09 12:24:28 +0100

    change inverted controls

--- car.js
+++ car.js
@@ -44,16 +44,21 @@ class Car {
 			this.speed = 0;
 		}
 
-		if (this.controls.left) {
-			this.angle += 0.03
-		}
+		//Flip the inverted controls
+		if (this.speed != 0) {
+			const flip = this.speed > 0 ? 1 : -1;
 
-		if (this.controls.right) {
-			this.angle -= 0.03
+			if (this.controls.left) {
+				this.angle += 0.03 * flip
+			}
+
+			if (this.controls.right) {
+				this.angle -= 0.03 * flip
+			}
+
+			this.x -= Math.sin(this.angle) * this.speed;
+			this.y -= Math.cos(this.angle) * this.speed;
 		}
-		
-		this.x -= Math.sin(this.angle)*this.speed;
-		this.y -= Math.cos(this.angle)*this.speed;
 	}
 
 	draw(context) {
