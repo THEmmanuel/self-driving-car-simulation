0edebbfffb8f5c3b2505699f3516f3bf3831d7bd
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-10 17:16:45 +0100

    add traffic

--- index.js
+++ index.js
@@ -7,16 +7,25 @@ const context = canvas.getContext('2d');
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 //get car
 const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const traffic = [
+	new Car(road.getLaneCenter(1), -100, 30, 50)
+];
 
 const animate = () => {
+	for (let index = 0; index < traffic.length; index++) {
+		traffic[index].update(road.borders);
+	}
 	car.update(road.borders);
-	
+
 	canvas.height = window.innerHeight;
 	context.save();
 
 	context.translate(0, -car.y+canvas.height * 0.7);
 
 	road.draw(context);
+	for (let index = 0; index < traffic.length; index++) {
+		traffic[index].draw(context);
+	}
 	car.draw(context);
 
 	context.restore();
