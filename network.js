e3ec275359fcd289bc9de46300d594d4fff5b244
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-11 01:48:57 +0100

    Add neural network object

--- network.js
+++ network.js
@@ -1,4 +1,23 @@
-class Ne
+class NeuralNetwork {
+	constructor(neuronCounts) {
+		this.levels = [];
+		for (let i = 0; i < neuronCounts.length - 1; i++) {
+			this.levels.push(new Level(
+				neuronCounts[i], neuronCounts[i + 1]
+			));
+		}
+	}
+
+	static feedForward(givenInputs, network) {
+		let outputs = Level.feedForward(
+			givenInputs, networkk.levels[0]);
+		for (let i = 1; i < network.levels.length; i++) {
+			outputs = Level.feedForward(
+				outputs, network.levels[i]);
+		}
+		return outputs;
+	}
+}
 
 class Level {
 	constructor(inputCount, outputCount) {
@@ -37,9 +56,9 @@ class Level {
 				sum += level.inputs[j] * level.weights[j][i];
 			}
 
-			if (sum > level.biases[i]){
+			if (sum > level.biases[i]) {
 				level.outputs[i] = 1;
-			}else{
+			} else {
 				level.outputs[i] = 0;
 			}
 		}
