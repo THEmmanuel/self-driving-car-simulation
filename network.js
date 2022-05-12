bd77ac693de526009d1aacf0b7914c3d3c86b72d
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-12 22:12:42 +0100

    add genetic algorithms

--- network.js
+++ network.js
@@ -28,7 +28,7 @@ class NeuralNetwork {
 				)
 			}
 			for (let i = 0; i < level.weights.length; i++) {
-				for (let j = 0; j < level.weights.length; j++) {
+				for (let j = 0; j < level.weights[i]; j++) {
 					level.weights[i][j] = linearInterpolation(
 						level.weights[i][j],
 						Math.random() * 2 - 1,
