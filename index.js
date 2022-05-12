7e059f451d149986156be9220b533f99fa97b1e9
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-12 21:26:39 +0100

    Add cars to lanes

--- index.js
+++ index.js
@@ -21,10 +21,30 @@ const networkContext = networkCanvas.getContext('2d');
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 //get car
 const N = 100;
-const cars = generateCars(N)
+const cars = generateCars(N);
+let bestCar = cars[0];
+if (localStorage.getItem('bestBrain')) {
+	bestCar.brain = JSON.parse(
+		localStorage.getItem('bestBrain')
+	)
+}
+
 const traffic = [
-	new Car(road.getLaneCenter(1), -100, 30, 50, 'NO_KEYS', 2)
+	new Car(road.getLaneCenter(1), -100, 30, 50, 'NO_KEYS', 2),
+	new Car(road.getLaneCenter(0), -300, 30, 50, 'NO_KEYS', 2),
+	new Car(road.getLaneCenter(2), -300, 30, 50, 'NO_KEYS', 2)
 ];
+
+const save = () => {
+	localStorage.setItem(
+		'bestBrain',
+		JSON.stringify(bestCar.brain)
+	);
+};
+
+const discard = () => {
+	localStorage.removeItem('bestBrain');
+};
 
 const animate = (time) => {
 	for (let index = 0; index < traffic.length; index++) {
@@ -36,7 +56,7 @@ const animate = (time) => {
 	}
 
 	// Create a new array with the cars' y value
-	const bestCar = cars.find(
+	bestCar = cars.find(
 		c => c.y === Math.min(
 			...cars.map(c => c.y)
 		));
