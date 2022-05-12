8da4171210b08df6029d492f9df5aeadf4a9f065
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-05-12 20:45:04 +0100

    Add new cars

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ carCanvas.width = 200;
 const networkCanvas = document.getElementById('networkCanvas')
 networkCanvas.width = 300;
 
+const generateCars = (n) => {
+	const cars = [];
+	for (let i = 1; i <= N; i++) {
+		cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, 'AI'))
+	}
+	return cars;
+}
+
 
 //get drawing context
 const carContext = carCanvas.getContext('2d');
@@ -12,7 +20,8 @@ const networkContext = networkCanvas.getContext('2d');
 //get road
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 //get car
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'AI');
+const N = 100;
+const cars = generateCars(N)
 const traffic = [
 	new Car(road.getLaneCenter(1), -100, 30, 50, 'NO_KEYS', 2)
 ];
@@ -21,26 +30,32 @@ const animate = (time) => {
 	for (let index = 0; index < traffic.length; index++) {
 		traffic[index].update(road.borders, []);
 	}
-	car.update(road.borders, traffic);
+	for (let i = 0; i < cars.length; i++) {
+		cars[i].update(road.borders, traffic);
+
+	}
 
 	carCanvas.height = window.innerHeight;
 	networkCanvas.height = window.innerHeight;
 
 	carContext.save();
 
-	carContext.translate(0, -car.y+carCanvas.height * 0.7);
+	carContext.translate(0, -cars[0].y + carCanvas.height * 0.7);
 
 	road.draw(carContext);
 	for (let index = 0; index < traffic.length; index++) {
 		traffic[index].draw(carContext, 'red');
 	}
-	car.draw(carContext, 'blue');
+
+	for (let i = 0; i < cars.length; i++) {
+		cars[i].draw(carContext, 'blue');
+	}
 
 	carContext.restore();
 
-	networkContext.lineDashOffset = -time/50;
+	networkContext.lineDashOffset = -time / 50;
 
-	Visualizer.drawNetwork(networkContext, car.brain);
+	Visualizer.drawNetwork(networkContext, cars[0].brain);
 	//Calls the animate 
 	requestAnimationFrame(animate);
 }
